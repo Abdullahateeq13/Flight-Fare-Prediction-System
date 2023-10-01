20f89e4c79bdef5213149b08c118112e7372a892
Abdullah Ateeq <abdullahateeq852@gmail.com>
2023-10-01 13:21:44 +0530

    data ingestion and .gitignore updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTrasformation
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -22,7 +26,7 @@ class DataIngestion:
         logging.info("Data ingestion initiated")
 
         try:
-            df = pd.read_csv("notebook\\data\\final_train_df.csv")
+            df = pd.read_excel("notebook\\data\\Data_Train.xlsx")
             logging.info('Read the dataset as a dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -31,7 +35,7 @@ class DataIngestion:
 
             logging.info("Train Test Split Initiated.")
 
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=40)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
 
@@ -51,5 +55,11 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
-                                  
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTrasformation()
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+    # print(train_arr)
+    # print(test_arr)                             
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
